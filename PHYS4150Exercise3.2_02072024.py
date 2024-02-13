bd2c4155ada221feebb9abf5a16a8710947d3968
JoePythonGitHub <158098302+JoePythonGitHub@users.noreply.github.com>
2024-02-13 12:30:34 -0500

    Add files via upload
    Signed-off-by: JoePythonGitHub <158098302+JoePythonGitHub@users.noreply.github.com>

---
+++ PHYS4150Exercise3.2_02072024.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+theta = np.linspace(0,2*np.pi)
+x = 2*np.cos(theta) + np.cos(2*(theta))
+y = 2*np.sin(theta) + np.sin(2*(theta))
+
+plt.plot(x,y)
+plt.show()
+
+theta = np.linspace(0,10*np.pi)
+r = theta**2
+x = r*np.cos(theta)
+y = r*np.sin(theta)
+
+plt.plot(x,y)
+plt.show()
+
+theta = np.linspace(0,24*np.pi)
+r = np.exp(np.cos(theta)) - 2*np.cos(4*(theta)) + (np.sin((theta)/2))**5
+x = r*np.cos(theta)
+y = r*np.sin(theta)
+
+plt.plot(x,y)
+plt.show()
+
